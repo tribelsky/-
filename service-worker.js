31dddc78356c93318d5e3f45f51584ac351b8f2c
tribelsky <160258663+tribelsky@users.noreply.github.com>
2026-07-29 20:27:10 +0300

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "mcm-shell-v1";
+const SHELL = ["./", "./manifest.webmanifest", "./mcm-icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
